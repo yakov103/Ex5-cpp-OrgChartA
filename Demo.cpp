7dbe743e1bbcbb06e6efd476389220ced514e8d2
yakov103 <yaakov103@gmail.com>
2022-05-21 08:21:36 -0700

    demo is ok

--- Demo.cpp
+++ Demo.cpp
@@ -15,55 +15,42 @@ using namespace std;
 using namespace ariel;
 
 int main() {
-    printf("here\n");
-    OrgChart organization;
+  OrgChart organization;
+  organization.add_root("CEO")
+      .add_sub("CEO", "CTO")         // Now the CTO is subordinate to the CEO
+      .add_sub("CEO", "CFO")         // Now the CFO is subordinate to the CEO
+      .add_sub("CEO", "COO")         // Now the COO is subordinate to the CEO
+      .add_sub("CTO", "VP_SW") // Now the VP Software is subordinate to the CTO
+      .add_sub("COO", "VP_BI");      // Now the VP_BI is subordinate to the COO
 
-    organization.add_root("CEO")
-            .add_sub("CEO", "CTO")         // Now the CTO is subordinate to the CEO
-            .add_sub("CEO", "CFO")         // Now the CFO is subordinate to the CEO
-            .add_sub("CEO", "COO")         // Now the COO is subordinate to the CEO
-            .add_sub("CTO", "VP_SW") // Now the VP Software is subordinate to the CTO
-            .add_sub("COO", "VP_BI");      // Now the VP_BI is subordinate to the COO
-printf("here2\n");
+  cout << organization << endl; /* Prints the org chart in a reasonable format. For example:
+       CEO
+       |--------|--------|
+       CTO      CFO      COO
+       |                 |
+       VP_SW             VP_BI
+ */
 
-//   printf("here2\n");
-//   cout << organization << endl; /* Prints the org chart in a reasonable format. For example:
-//        CEO
-//        |--------|--------|
-//        CTO      CFO      COO
-//        |                 |
-//        VP_SW             VP_BI
-//  */
+  for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
+  {
+    cout << (*it) << " " ;
+  } // prints: CEO CTO CFO COO VP_SW VP_BI
+  for (auto it = organization.begin_reverse_order(); it != organization.reverse_order(); ++it)
+  {
+    cout << (*it) << " " ;
+  } // prints: VP_SW VP_BI CTO CFO COO CEO
+  for (auto it=organization.begin_preorder(); it!=organization.end_preorder(); ++it) {
+    cout << (*it) << " " ;
+  }  // prints: CEO CTO VP_SW CFO COO VP_BI
 
-//    printf("here3\n");
-    for (auto it = organization.begin_level_order(); it != organization.end_level_order(); it++)
-    {
-        cout << (*it) << " " ;
+  for (auto element : organization)
+  { // this should work like level order
+    cout << element << " " ;
+  } // prints: CEO CTO CFO COO VP_SW VP_BI
 
-
-
-    } // prints: CEO CTO CFO COO VP_SW VP_BI
-    printf("here3\n");
-    printf("lllll\n");
-
-    for (auto it = organization.begin_reverse_order(); it != organization.reverse_order(); it++)
-    {
-        cout << (*it) << " " ;
-    }
-  printf("here4\n");
-    // prints: VP_SW VP_BI CTO CFO COO CEO
-   for (auto it=organization.begin_preorder(); it!=organization.end_preorder(); ++it) {
-       cout << (*it) << " " ;
-   }  // prints: CEO CTO VP_SW CFO COO VP_BI
-
-//    for (auto element : organization)
-//    { // this should work like level order
-//        cout << element << " " ;
-//    } // prints: CEO CTO CFO COO VP_SW VP_BI
-    printf("\nhere4\n");
-    // demonstrate the arrow operator:
-//    for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
-//    {
-//        cout << it->size() << " " ;
-//    } // prints: 3 3 3 3 5 5
+  // demonstrate the arrow operator:
+  for (auto it = organization.begin_level_order(); it != organization.end_level_order(); ++it)
+  {
+    cout << it->size() << " " ;
+  } // prints: 3 3 3 3 5 5
 }
